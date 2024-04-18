15a88f5da4baee4d21675cf08cfa2ada4b57f13c
devsherm <136485191+devsherm@users.noreply.github.com>
2024-04-18 15:06:31 -0500

    Create Devise User
    > rails g devise User name email
    > rails db:migrate

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise  :database_authenticatable,
+          :registerable,
+          :recoverable,
+          :rememberable,
+          :validatable#,
+          # :confirmable, # Database is set up, but you need to set up the mailer, if you want
+
+end
