fb883d9d752ccf3e29bf6fddaf779d7bd516f300
devsherm <136485191+devsherm@users.noreply.github.com>
2024-04-18 16:51:07 -0500

    Prompt user for name. First user with name is Admin
    > rails g migration AddAdminToUsers admin:boolean
    > rails db:migrate
    > rails g scaffold_controller Users name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_18_195616) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_18_212641) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_18_195616) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
