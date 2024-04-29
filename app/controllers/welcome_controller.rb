e8989f416e7bdfc76ccfd5dc88ddecb5e54823ad
Mohammad Faris <mo7amad84@gmail.com>
2024-04-29 09:10:12 -0700

    add devise views

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,9 @@
 class WelcomeController < ApplicationController
-  before_action :authenticate_user!, only: %i[ index, spring_24_junior_rails_developer ] 
+  before_action :authenticate_user!, only: [ :index, :spring_24_junior_rails_developer ] 
 
+  def index
+    
+  end
   def spring_24_junior_rails_developer
   end
 end
