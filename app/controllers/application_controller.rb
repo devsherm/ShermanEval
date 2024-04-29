2e95692d5f7997f4eaccbb2d02cb5d548f769ae2
Mohammad Faris <mo7amad84@gmail.com>
2024-04-29 09:21:47 -0700

    add full name to the registration page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(user)
     # Check if any critical profile information is missing
@@ -10,4 +11,11 @@ class ApplicationController < ActionController::Base
       super
     end
   end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+  
 end
