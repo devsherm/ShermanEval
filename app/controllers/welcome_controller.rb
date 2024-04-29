3ed0daf4a741b8ce9a0a1e434a8c14a2af75ad8d
Mohammad Faris <mo7amad84@gmail.com>
2024-04-29 09:07:05 -0700

    authentication

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
+  before_action :authenticate_user!, only: %i[ index, spring_24_junior_rails_developer ] 
 
   def spring_24_junior_rails_developer
   end
