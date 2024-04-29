604b46464c9b9e9e645e57f62fa9b171e47cdc7e
artdevil <adhiguna.sabril@gmail.com>
2024-04-29 16:53:34 +0700

    fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,19 @@ Rails.application.routes.draw do
   
   devise_for :users
 
+  authenticated :user, ->(user) { user.admin? } do
+    root 'admin/applicants#index', as: :admin_root
+  end
+
+  authenticated :user do
+    root 'applicants#show', as: :authenticated_root
+  end
+
   namespace :admin do
     resources :applicants
   end
 
+  resources :users, only: %i[edit update]
   resource :applicants, only: %i[:show update]
 
   root 'applicants#show'
