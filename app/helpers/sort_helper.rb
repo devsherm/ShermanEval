1cb582a5da558ef7773fefeed034a5047dcb8c17
artdevil <adhiguna.sabril@gmail.com>
2024-04-29 19:00:34 +0700

    removing unused code

--- app/helpers/sort_helper.rb
+++
@@ -1,9 +0,0 @@
-module SortHelper
-  def sort_helper(title)
-    link_to "#{title.upcase} " + (params[:sort_by] == title ? (params[:sort_order] == "asc" ? "▲" : "▼") : ""), admin_applicants_path(search: params[:search], sort_by: title, sort_order: (params[:sort_by] == title && params[:sort_order] == "asc") ? "desc" : "asc")
-  end
-
-  def search_helper
-    admin_applicants_path(search: params[:search], sort_order: params[:sort_order], sort_by: params[:sort_by])
-  end
-end
