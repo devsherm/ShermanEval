4ab84b75339c48ba918c8f0a4038b9621179345f
dev-invasso <dev@invasso.com>
2024-05-01 11:39:17 +0300

    Fix rubocop Linter errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-
   def not_found
     render file: "#{Rails.root}/public/404.html", status: :not_found
   end
