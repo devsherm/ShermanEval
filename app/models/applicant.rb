08452d922d8cdf68b4dd9cc39800ffb77665d242
Gabe Nielsen <101427061+NielsenG62@users.noreply.github.com>
2024-05-01 13:43:02 -0700

    add evaluation score to applicants, visible to admin

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -4,10 +4,17 @@ class Applicant < ApplicationRecord
     validates :experience, presence: true
     validates :availability, presence: true
     serialize :availability, Array
-    before_save(:titleize_name)
+    before_save(:titleize_name, :tally_score)
 
     private
     def titleize_name
         self.name = self.name.titleize
+    end
+
+    def tally_score
+        score = 0
+        score += 2 if self.experience == 'Some'
+        score += 4 if self.experience == 'Lots'
+        self.score = score + availability.size
     end
 end
---
+++ db/migrate/20240501200040_add_score_to_applicant.rb
@@ -0,0 +1,5 @@
+class AddScoreToApplicant < ActiveRecord::Migration[7.0]
+  def change
+    add_column :applicants, :score, :integer
+  end
+end
