863d9d94882d4da2b6126ca75794fb2f7082a477
Stefanie <stefanieheinz0@gmail.com>
2024-05-01 13:28:08 -0600

    validations, sanitization, and clean up

--- app/models/user_application.rb
+++ app/models/user_application.rb
@@ -1,9 +1,16 @@
 class UserApplication < ApplicationRecord
+  AVAILABLE_SKILLS = ['ruby', 'react', 'vue_js', 'python'].freeze
+
   belongs_to :user
   validates :user, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :skills, presence: true
+
+  validates :first_name, format: { with: /\A[a-zA-Z\s\-]+\z/, message: "must contain only valid characters (letters, spaces, and hyphens only permitted)" }
+  validates :last_name, format: { with: /\A[a-zA-Z\s\-]+\z/, message: "must contain only valid characters (letters, spaces, and hyphens only permitted)" }
+
+  validates :skills, inclusion: { in: AVAILABLE_SKILLS, message: "is not a valid skill" }
 
   enum gender: { other: 0, male: 1, female: 2 }
 
@@ -15,6 +22,12 @@ class UserApplication < ApplicationRecord
 
   def self.ransackable_attributes(auth_object = nil)
     %w[score first_name last_name]
+  end
+
+  def self.skills_for_select
+    AVAILABLE_SKILLS.map do |skill|
+      [skill.titleize, skill]
+    end
   end
 
   def full_name
