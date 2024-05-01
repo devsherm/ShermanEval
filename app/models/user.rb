6c5e3741c96ff654ce6cccda9b55fb2a39d1349e
Stefanie <stefanieheinz0@gmail.com>
2024-05-01 15:57:15 -0600

    user email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ApplicationRecord
           # :confirmable, # Database is set up, but you need to set up the mailer, if you want
           :validatable
 
+  validates :email, presence: true, format: { with: /\A[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}\z/, message: "Please enter a valid email address." }  
+  
+  has_many :applications, class_name: "UserApplication"
+
   def self.ransackable_attributes(auth_object = nil)
     %w[email name]
   end
