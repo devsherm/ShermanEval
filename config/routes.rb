8c0793981847212599e7ef17642639ec060bd783
Adam <73717269+adamabad@users.noreply.github.com>
2024-05-03 03:39:50 -0400

    Update routes.rb
    - Updated routes.rb
      - Added admin authenticated paths: admin, update, email
      - Added general paths: questionnaire, submit, viewer
      - Added applicants resource
      - Updated root to redirect to spring_24_junior_rails_developer landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
+  authenticated :user, ->(user) { user.admin? } do
+    get 'admin', to: 'admin_view#admin_view' 
+    patch 'update', to: 'admin_view#update'
+    post 'email', to: 'admin_view#email'
+  end
+  get 'questionnaire', to: 'questionnaire#questionnaire'
+  post 'submit', to: 'questionnaire#create'
+  get 'viewer', to: 'questionnaire#viewer'
   devise_for :users
   resources :users, only: %i[edit update]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  get 'welcome/spring_24_junior_rails_developer'
+  resources :applicants
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # get 'welcome/spring_24_junior_rails_developer'
 
   # Defines the root path route ("/")
-  root 'welcome#index'
+  root 'welcome#spring_24_junior_rails_developer'
 end
