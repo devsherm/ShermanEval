0993677233740be969e70bfaa8b031bb4b0f9d61
Roman Bondarenko <bondarenko.roman@gmail.com>
2024-05-06 18:05:27 -0400

    NO-REF: new applicant creation use case implementation

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -1,5 +1,5 @@
 class ApplicantsController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[new]
+  skip_before_action :authenticate_user!, only: %i[new create]
 
   def index
     @applicants = Applicant.all
@@ -10,14 +10,27 @@ class ApplicantsController < ApplicationController
   end
 
   def new
-    @applicant = Applicant.new
+    @questions = Question.all
+    @user = User.new
+    @applicant = Applicant.new(user: @user, answers: @questions.map { |q| Answer.new(question: q) })
   end
 
   def create
-    @applicant = Applicant.new(applicant_params)
+    # TODO: remove this hack
+    params[:applicant][:answers_attributes].each_value { |v| if v[:value].is_a?(Array) then v[:value] = v[:value].compact_blank.join("\n") else v end  }
 
-    if @applicant.save
-      redirect_to @applicant
+    @user = User.new(params.require(:applicant).require(:user).permit(:name, :email, :password))
+    # params.require(:applicant).permit(user: [:name, :email], answers_attributes: [:id, :value])
+    p = params.require(:applicant).permit(answers_attributes: [:value, :id, :question_id])
+    @applicant = Applicant.new(user: @user, answers_attributes: p[:answers_attributes])
+    if @user.save
+      #redirect_to user_url(@user), notice: "User was successfully created."
+      @applicant.user = @user
+      if @applicant.save
+        redirect_to @applicant
+      else
+        render :new, status: :unprocessable_entity
+      end
     else
       render :new, status: :unprocessable_entity
     end
@@ -25,15 +38,17 @@ class ApplicantsController < ApplicationController
 
   def edit
     @applicant = Applicant.find(params[:id])
+    @questions = Question.all
   end
 
   def update
     @applicant = Applicant.find(params[:id])
 
     #debugger
+    # TODO: remove this hack
     params[:applicant][:answers_attributes].each_value { |v| if v[:value].is_a?(Array) then v[:value] = v[:value].compact_blank.join("\n") else v end  }
-    p = params.require(:applicant).permit(answers_attributes: [:id, :value])
-    if @applicant.update(p)
+
+    if @applicant.update(applicant_params)
       redirect_to @applicant
     else
       render :edit, status: :unprocessable_entity
@@ -42,6 +57,6 @@ class ApplicantsController < ApplicationController
 
   private
     def applicant_params
-      params.require(:applicant).permit(answers_attributes: [:value, :id])
+      params.require(:applicant).permit(:user, answers_attributes: [:id, :value])
     end
 end
