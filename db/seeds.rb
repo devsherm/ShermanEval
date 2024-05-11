b4e3d9123df48f1a58a3b601103757807b2b48c7
elasticspoon <mail@yuribocharov.dev>
2024-05-11 15:56:19 -0400

    fix: seed file idempotency
    Seeds would not work if existing records were in the
    database with matching emails.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,19 @@
-User.create(email: 'admin@example.com', password: 'password!', password_confirmation: 'password!', admin: true)
-User.create(email: 'applicant@example.com', password: 'password!', password_confirmation: 'password!', admin: false)
-User.create(email: 'applicant2@example.com', password: 'password!', password_confirmation: 'password!', admin: false)
+admin = User.find_or_initialize_by(email: 'admin@example.com')
+admin.update(password: 'password!', password_confirmation: 'password!', admin: true)
+
+applicant1 = User.find_or_initialize_by(email: 'applicant1@example.com')
+applicant1.update(password: 'password!', password_confirmation: 'password!', admin: false)
+
+applicant2 = User.find_or_initialize_by(email: 'applicant2@example.com')
+applicant2.update(password: 'password!', password_confirmation: 'password!', admin: false)
+
+[applicant1, applicant2].each do |user|
+  5.times do
+    admin.recieved_applications << UserApplication.new(
+      my_level: UserApplication.my_levels.keys.sample,
+      fav_hobby: 'reading',
+      scratch_before: [true, false].sample,
+      user:
+    )
+  end
+end
